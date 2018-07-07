58a5c9f116a70bf518a7c87cd68c5c7c032efc93
Jonah <idrissberchil25@gmail.com>
2018-07-07 23:38:10 +0100

    Add files via upload

---
+++ RPC.py
@@ -0,0 +1,57 @@
+from random import choice
+from time import sleep
+
+'''
+
+'''
+
+RPC_Choices = ["Rock","Paper","Scissors"]
+Player_Score = 0
+Computer_Score = 0
+Round = 1
+#Player Choice at Line 30.
+
+
+
+if input("Would you like to play Rock Paper Scissors with a bot ?: \n").upper() == "YES":
+    sleep(0.5)
+    print("Alright Lets Start ! (...)")
+    sleep(2)
+else:
+    if input("Are you Sure ? \n").upper() == "YES":
+        sleep(0.5)
+        print("Ok then Shutting down ...")
+        sleep(2)
+        quit()
+    else:
+        sleep(0.5)
+        print("Lets play the game then...")
+        sleep(2)
+
+Player = input("Please pick a Username: ")
+
+# vvvvvvv Game Starts from here vvvvvvv
+while True:
+    sleep(0.5)
+    print("Round "+str(Round)+", GO!")
+    Player_Choice = input("Rock, paper or scissors ?\n")
+    Computer_Choice = choice(RPC_Choices)
+    sleep(0.5)
+    if (Player_Choice.upper() ==  "ROCK" and Computer_Choice.upper() == "PAPER") or (Player_Choice.upper() == "PAPER" and Computer_Choice.upper() == "SCISSORS") or (Player_Choice.upper() == "SCISSORS" and Computer_Choice.upper() == "ROCK"):
+        Computer_Score += 1
+        print(Player+" "+str(Player_Score)+" -(Computer Won)- "+str(Computer_Score)+" Computer"+"\n")
+    elif (Player_Choice.upper() ==  "ROCK" and Computer_Choice.upper() == "SCISSORS") or (Player_Choice.upper() == "PAPER" and Computer_Choice.upper() == "ROCK") or (Player_Choice.upper() == "SCISSORS" and Computer_Choice.upper() == "PAPER"):
+        Player_Score += 1
+        print(Player+" "+str(Player_Score)+" -(You Won)- "+str(Computer_Score)+" Computer"+"\n")
+    elif Player_Choice.upper() == Computer_Choice.upper():
+        print(Player+" "+str(Player_Score)+" -(its a Tie)- "+str(Computer_Score)+" Computer"+"\n")
+    Round +=1
+    
+    if Round == 15:
+        if Player_Score > Computer_Score:
+            print("---------You WIN !!---------")
+        elif Player_Score < Computer_Score:
+            print("---------You LOSE !!---------")
+        elif Player_Score == Computer_Score:
+            print("---------Its a Tie !!---------")
+        break
